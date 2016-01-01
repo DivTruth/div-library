35a90aa5faa3f0798b572969d59e4deb70a7fe44
Nick Worth <nick@divtruth.com>
2016-01-01 16:35:34 -0700

    Fix setup_capabilities(), not working properly

--- includes/class-div-roles.php
+++ includes/class-div-roles.php
@@ -83,64 +83,38 @@ class DIV_Role {
 	 * @param string/array
 	 * @return array
 	 */
-	static function setup_capabilities($c=""){
-		// Set default clone permissions in General Settings or with 'div_default_role' filter
+	public function setup_capabilities($caps=""){
+		/* Set default clone permissions in General Settings or with 'div_default_role' filter */
 		$default_role = apply_filters( 'div_default_role', get_option( 'default_role', 'subscriber' ) );
 
-		// If array, look for inherited roles, and setup any custom capabilities
-        if (  is_array($c) ) {
-	        // Check for inherited roles
-	        foreach ($c as $capability => $v) {
-				$roles = self::get_roles(1);
-				$admin_role = get_role('administrator');
+		/* If array, look for inherited roles, then setup any custom capabilities in addition */
+        if ( is_array($caps) ) {
+	        /* Iterate through all passed capabilities */
+	        foreach ($caps as $i => $capability) {
 
-				if(in_array($capability, $roles)){
-					unset($c[$capability]); //remove role
+		        /* Checking for inherited roles */
+				$all_roles = self::get_roles(1);
+				if(in_array($capability, $all_roles)){
 					
-					// Add role's capabilities
-					$inherited = get_role($capability)->capabilities;
-					$c['inherited'] = $capability;
-					$c = array_merge($c, $inherited);
+					/* Add role's capabilities */
+					$inherited = get_role($all_roles[$i])->capabilities;
+					$caps['inherited'] = $i; # Indicate inheritance
+					$caps = array_merge($caps, $inherited);
 				} else {
-					if(!array_key_exists ( $capability, $admin_role->capabilities)){
-						$admin_role->add_cap($capability);
-					}
+					$caps[$capability] = $this->capabilities[$capability] = 1;
 				}
+				unset($caps[$i]); # remove role from list of capabilities
 			}
 		}
 
-		// If not provided or string, then inherit capabilities from default or declared role
-		if ( empty($c) || is_string($c)) {
-        	$default = ( empty($c) ) ? get_role($default_role) : get_role($c);
-        	$c = $default->capabilities;
-        	$c['inherited'] = $default->name;
+		/* If not provided or string, then inherit capabilities from default or declared role */
+		if ( empty($caps) || is_string($caps)) {
+        	$default = ( empty($caps) ) ? get_role($default_role) : get_role($caps);
+        	$caps = $default->capabilities;
+        	$caps['inherited'] = $default->name; # Indicate inheritance
         }
         
-       	return $c;
-	}
-
-	/**
-	 * Add the capabilities to defined roles
-	 * @access public
-	 * @param string/array
-	 */
-	public function add_cap($c){
-		$admin_role = get_role('administrator');
-		$admin_role->add_cap($c);
-		foreach ($this->roles as $name => $role) {
-			$role->add_cap($c);
-		}
-	}
-
-	/**
-	 * Remove capabilities from defined roles
-	 * @access public
-	 * @param string/array
-	 */
-	public function remove_cap($c){
-		foreach ($this->roles as $name => $role) {
-			$role->remove_cap($c);
-		}
+       	return $caps;
 	}
 
 	/**
